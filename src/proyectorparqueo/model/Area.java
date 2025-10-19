a6daaa19713b2a28932c1a468cc69a27624774ee
DIEGO <DIEGO@diego0705>
2025-10-18 20:19:03 -0600

    primero commit del proyecto

---
+++ src/proyectorparqueo/model/Area.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectorparqueo.model;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class Area {
+    // Atributos
+    public String id;
+    public String nombre;
+    public int capacidad;
+    public String tipoVehiculo;
+    public int ocupados;
+    
+    // Constructor
+    public Area(String id, String nombre, int capacidad, String tipoVehiculo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.tipoVehiculo = tipoVehiculo;
+        this.ocupados = 0;
+    }
+
+    // Métodos Get y Set
+    public String getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getCapacidad() {
+        return capacidad;
+    }
+    public String getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+    public int getOcupados() {
+        return ocupados;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    public void setTipoVehiculo(String tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+    public void setOcupados(int ocupados) {
+        this.ocupados = ocupados;
+    }
+    // Método para verificar si el área está llena
+    public boolean estaLlena() {
+        return ocupados >= capacidad;
+    }
+}
