475b31bc6ef18ced95bd6601606764466659aa6a
Miguel Fernando <mztiquinm@miumg.edu.gt>
2025-10-20 19:49:16 -0600

    Agregar clase Vehiculo y prueba de funcionamiento

---
+++ src/proyectorparqueo/model/pruebaVehiculo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectorparqueo.model;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class pruebaVehiculo {
+    public static void main(String[] args)
+{   
+    vehiculo v = new vehiculo("P123B", "Juan Perez", "Carro", "Plan", false);
+    
+    v.mostrarDatos();
+    v.cancelarPlan();
+    System.out.println("Plan Activo? " + v.isPlanActivo());
+   }
+    
+}
---
+++ src/proyectorparqueo/model/vehiculo.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectorparqueo.model;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class vehiculo {
+    public String placa;
+    public String propietario;
+    public String categoria;
+    public String tipoPlan;
+    public boolean planActivo;
+
+    public vehiculo(String placa, String propietario, String categoria, String tipoPlan, boolean planActivo) {
+        this.placa = placa;
+        this.propietario = propietario;
+        this.categoria = categoria;
+        this.tipoPlan = tipoPlan;
+        this.planActivo = planActivo;
+    }
+    public String getPlaca() {
+        return placa;
+    }
+    public String getPropietario() {
+        return propietario;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public String getTipoPlan() {
+        return tipoPlan;
+    }
+    public boolean isPlanActivo() {
+        return planActivo;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public void setTipoPlan(String tipoPlan) {
+        this.tipoPlan = tipoPlan;
+    }
+    public void setPlanActivo(boolean planActivo) {
+        this.planActivo = planActivo;
+    }
+    
+    public void cancelarPlan(){
+        this.planActivo = false;
+    }
+    public void reactivarPlan(){
+        this.planActivo = false;
+    }
+    
+    public void mostrarDatos(){
+        System.out.println("Placa: " + placa);
+        System.out.println("Propietario:" + propietario);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Tipo de PLan: " + tipoPlan);
+        System.out.println("Plan Activo: " + planActivo);
+    }
+    
+}
